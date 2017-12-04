d6db5db9bd51c5b2242da4716170999741bdaf99
YuanYuLing110 <YuanYuLing@126.com>
2017-12-04 15:13:22 +0800

    发布初始

---
+++ TaoPiaoPiao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaoPiaoPiao.MvcApplication" Language="C#" %>
---
+++ TaoPiaoPiao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaoPiaoPiao.Startup))]
+namespace TaoPiaoPiao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
